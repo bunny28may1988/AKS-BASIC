eb662587cc991c9d2fb9032a74fd7b27a075d95c
bunny28may1988 <bunny28may1988@gmail.com>
2020-09-07 23:21:43 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -37,6 +37,7 @@ variable log_analytics_workspace_sku {
     default = "PerGB2018"
 }
 variable "azure_client_id" {}
+variable "azure_client_secret" {}
 variable "azure_tenant_id" {}
 variable "azure_subscription_id" {}
 variable "azure_client_certificate_password"{}
